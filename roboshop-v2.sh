4b46320e2fb30904e46ecd91a12a4359281c5888
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-09 14:44:39 +0530

    api docs added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -3,8 +3,8 @@
 #export PATH=$PATH:/usr/local/bin
 
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z07086101C1CVP7AT2UK4" # replace with your zone ID
-DOMAIN_NAME="daws90s.shop" # replace with your domain name
+ZONE_ID="Z05477002C2Z6SK9SKMK6" # replace with your zone ID
+DOMAIN_NAME="arjundevops.xyz" # replace with your domain name
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
